923d1d3e849d5a2aeb6d6870576a6c775a13df6d
Winston Leung <wleung85@uwo.ca>
2021-10-24 19:07:37 -0400

    Connected front-end to deployed contract

---
+++ frontend/src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xa49c322db25c1DbDAbAD136b551ef954b8B1cB0b';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
